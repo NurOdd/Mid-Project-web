aeb10f9d0214e1c7cb72013ca55c47494cc0dde5
Nuria Rodriguez <nur.rodga@gmail.com>
2022-10-07 12:46:39 +0200

    add fetch DOM
    a hard journey to fetch

--- js/index.js
+++ js/index.js
@@ -8,7 +8,18 @@ return data;
 
 const textInsert = async() =>{
     const getData = await fetchFunction();
- 
+
+    for (let i = 0; i < 3; i++) {
+        const post = getData[i];
+
+    document.querySelector(`#titleProject${i+1}`).innerHTML=post.title
+
+    document.querySelector(`#typologyProject${i+1}`).innerHTML=post.body
+
+    
+
+
+    }
 
 }
 
@@ -21,12 +32,15 @@ textInsert()
 
 //agafa la info del API (json)
 //guardar a un lloc*
-
 //dir-li a la maquina a on es*
- 
+ // 3 post
+// escriure dades a diferents elements (DOM)
+/*p = projectTitle
 
-//tipus de data que vull
-// insertar info a la tarjeta
+
+*/
+
+// insertar info a *a tarjeta
 
 
 // https://jsonplaceholder.typicode.com/posts
