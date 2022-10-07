36bc7f15e2a9cefb38e015fff5ba6d6196a9e881
Nuria Rodriguez <nur.rodga@gmail.com>
2022-10-07 10:11:15 +0200

    add newsletter form
    now you can join our sect!
    One of US! One of US!

---
+++ js/newsletterform.js
@@ -0,0 +1,14 @@
+//objectivo> inputs en consola + !refresh
+const newsletterForm=document.querySelector(".newsletterForm")
+
+// let form = document.forms['contactForm'];
+
+newsletterForm.onsubmit=function(e){
+    e.preventDefault();
+// console.log("hola")
+
+const newSubscriber = document.querySelector("#inputEmail").value;
+
+console.log("One of Us! One of Us!")
+alert("One of Us! One of Us!")
+}
